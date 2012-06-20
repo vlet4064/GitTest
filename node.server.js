9e7d32a0a81865134923be5b115a109ca10801c1
unknown <by.kim@bykim-a515.LGE.NET>
2012-06-20 10:17:16 +0900

    First File

---
+++ node.server.js
@@ -0,0 +1,11 @@
+var http = require('http');
+
+http.createServer(function (request, response) {
+	response.writeHead(200, {'Content-Type': 'text/html'});
+	response.end('<h1>Node.js Test Server ..!</h1>');
+}).listen(52273, function () {
+	console.log('Server running at http://127.0.0.1:52273/');
+});
+
+
+
